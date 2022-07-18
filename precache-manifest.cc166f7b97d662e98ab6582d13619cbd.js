cff1f8314fb47405ebabbbdfe3ade301996f254a
iptrackertutorial <akshat2032001@gmail.com>
2022-07-17 20:59:06 -0700

    Updates

---
+++ precache-manifest.cc166f7b97d662e98ab6582d13619cbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aa2164b250f7c8682dd",
+    "url": "/CipherSchool_todolist/static/css/main.58b5353c.chunk.css"
+  },
+  {
+    "revision": "2aa2164b250f7c8682dd",
+    "url": "/CipherSchool_todolist/static/js/main.2aa2164b.chunk.js"
+  },
+  {
+    "revision": "adda249f39898ab2e188",
+    "url": "/CipherSchool_todolist/static/js/1.adda249f.chunk.js"
+  },
+  {
+    "revision": "4eb7466860236f7891fc",
+    "url": "/CipherSchool_todolist/static/js/runtime~main.4eb74668.js"
+  },
+  {
+    "revision": "399c359a4b06a61dc4f6b21f004f3cde",
+    "url": "/CipherSchool_todolist/index.html"
+  }
+];
